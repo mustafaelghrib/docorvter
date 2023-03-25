aa51578873dddbc0aa26c5d39e2d97c238e4bbc9
mstva <mustafa.abdulluh@gmail.com>
2023-03-25 04:04:33 +0200

    added convert_html_file as a celery task

---
+++ backend/api/converter/tasks.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+from datetime import datetime
+
+from celery import shared_task
+from django.conf import settings
+
+from .document_converter.converter.html_converter import HtmlConverter
+from ..files.models import File
+
+
+@shared_task
+def convert_html_file(file_id):
+
+    file = File.objects.get(file_id=file_id)
+
+    output_file = f"{settings.MEDIA_ROOT}/out.pdf"
+
+    html_converter = HtmlConverter()
+    html_converter.convert(file=file.html_file.path, output=output_file)
+
+    file.converted_at = datetime.now()
+
+    HTML_FILES_DIR = f'{settings.MEDIA_ROOT}/html_files'
+    PDF_FILES_DIR = f'{settings.MEDIA_ROOT}/pdf_files'
+
+    new_html_file = f'{HTML_FILES_DIR}/{file.file_id}.html'
+    os.rename(file.html_file.path, new_html_file)
+    file.html_file = new_html_file
+
+    if not os.path.exists(PDF_FILES_DIR):
+        os.makedirs(PDF_FILES_DIR)
+
+    new_output_file = f"{PDF_FILES_DIR}/{file.file_id}.pdf"
+    shutil.copy(output_file, new_output_file)
+    file.pdf_file = new_output_file
+
+    file.save()
+
+    os.remove(output_file)
--- backend/api/converter/views.py
+++ backend/api/converter/views.py
@@ -1,19 +1,14 @@
-import os
-import shutil
 from datetime import datetime
 
-from django.conf import settings
 from rest_framework import views, status
 from rest_framework.response import Response
 
-from .document_converter.converter.html_converter import HtmlConverter
+from .tasks import convert_html_file
 from ..files.models import File
-from ..files.serializers import FileSerializer
 from ..users.jwt_auth import JWTAuthentication
 
 
 class HtmlConvertAPI(views.APIView):
-
     authentication_classes = [JWTAuthentication]
 
     @staticmethod
@@ -26,38 +21,13 @@ class HtmlConvertAPI(views.APIView):
             })
 
         html_file = request.data.get('html_file')
-
         file = File.objects.create(html_file=html_file, uploaded_at=datetime.now())
 
-        output_file = f"{settings.MEDIA_ROOT}/out.pdf"
-
-        html_converter = HtmlConverter()
-        html_converter.convert(file=file.html_file.path, output=output_file)
-
-        file.converted_at = datetime.now()
-
-        HTML_FILES_DIR = f'{settings.MEDIA_ROOT}/html_files'
-        PDF_FILES_DIR = f'{settings.MEDIA_ROOT}/pdf_files'
-
-        new_html_file = f'{HTML_FILES_DIR}/{file.file_id}.html'
-        os.rename(file.html_file.path, new_html_file)
-        file.html_file = new_html_file
-
-        if not os.path.exists(PDF_FILES_DIR):
-            os.makedirs(PDF_FILES_DIR)
-
-        new_output_file = f"{PDF_FILES_DIR}/{file.file_id}.pdf"
-        shutil.copy(output_file, new_output_file)
-        file.pdf_file = new_output_file
-
-        file.save()
-
-        os.remove(output_file)
+        convert_html_file.delay(file.file_id)
 
         return Response({
             "status": status.HTTP_200_OK,
-            "message": "File converted successfully",
-            "file": FileSerializer(file, context={"request": request}).data,
+            "message": "We are converting your file! Wait a moment!",
         })
 
     def post(self, request):
