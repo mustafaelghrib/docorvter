83e07aaf6a18df296c815cff20ddaeaf6a7b9b60
mstva <mustafa.abdulluh@gmail.com>
2023-03-26 23:03:20 +0200

    added docs for the api package

---
+++ backend/api/__init__.py
@@ -0,0 +1,11 @@
+"""
+This package contains the backend API endpoints.
+
+Sub-packages:
+    - [`converter`][backend.api.converter]:
+    Provide endpoints for converting files.
+    - [`files`][backend.api.files]:
+    Provide endpoints to manage files.
+    - [`users`][backend.api.users]:
+    Provide endpoints for user login and registration.
+"""
