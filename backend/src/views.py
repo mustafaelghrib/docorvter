3a4f8409c08a2de7b2f3cff24cb30fa2de2ec1b9
mstva <mustafa.abdulluh@gmail.com>
2023-03-26 19:49:42 +0200

    added docstring and refactor index_view

--- backend/src/views.py
+++ backend/src/views.py
@@ -1,8 +1,18 @@
-import os
+"""A module that contains the index view of the api url."""
+
 from decouple import config
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
-def index_view(request):
+def index_view(request: HttpRequest) -> HttpResponse:
+    """An index view for the root url of the api
+
+    Args:
+        request: the http request
+
+    Returns:
+        A welcome message in http response
+
+    """
     return HttpResponse(f"<h1>Welcome {config('DJANGO_SETTINGS_MODULE')}</h1>")
