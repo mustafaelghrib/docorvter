f969465dddb7025571449e88c4fb2b49ea8e25c5
mstva <mustafa.abdulluh@gmail.com>
2023-03-26 23:08:42 +0200

    added docs for storage module

--- backend/api/files/storage.py
+++ backend/api/files/storage.py
@@ -1,3 +1,10 @@
+"""
+A module that handle overriding files names.
+
+Classes:
+    - `OverwriteStorage`: A class that handle overriding files names
+"""
+
 import os
 
 from django.conf import settings
@@ -11,10 +18,13 @@ class OverwriteStorage(FileSystemStorage):
     This class inherits from Django's FileSystemStorage class and overrides the `get_available_name`
     method to check if a file with the same name already exists. If so, it deletes the old file before
     returning the name, allowing the new file to overwrite the old one.
+
+    Methods:
+        - `get_available_name(name, max_length)`: A method to get file name and override it
     """
 
     def get_available_name(self, name: str, max_length: Optional[int, None] = None) -> str:
-        """Returns a filename that's available for new content to be written to.
+        """Return a filename that's available for new content to be written to.
 
         If the file already exists, it is deleted to allow overwriting.
 
